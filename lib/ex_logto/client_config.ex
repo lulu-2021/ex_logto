87efce12912dbf7434c956c1537ad25872c1401e
netflakes <>
2024-06-14 18:42:36 +1000

    ensured all configuration is available - defaults set for optional ones

--- lib/ex_logto/client_config.ex
+++ lib/ex_logto/client_config.ex
@@ -1,38 +1,39 @@
 defmodule ExLogto.ClientConfig do
   @moduledoc """
+    all the configuration options handled in one place - the scopes
   """
+alias ElixirLS.LanguageServer.Application
 
-  @callback_url "http://lvh.me:4000/page/callback"
-  @authorization_endpoint "http://localhost:3001/oidc/auth/"
-  @token_endpoint "http://localhost:3001/oidc/token/"
-  @user_info_endpoint "http://localhost:3001/oidc/me/"
-  @end_session_endpoint "http://localhost:3001/oidc/session/end"
-  @post_logout_redirect_url "http://lvh.me:4000/"
-  @client_id "2a2yi37r08mv2ujr0dhf8"
-  @client_secret "qPl7Oc8Dxi1VGDDJwYpKjlL7WX99Xemj"
-  @prompt "consent"
+  @default_prompt "consent"
+  @default_scopes ["openid", "offline_access", "email", "profile"]
+  @default_resources []
 
   def signin_options(code_challenge, state, redirect_uri) do
     %{
       code_challenge: code_challenge,
-      authorization_endpoint: @authorization_endpoint,
-      client_id: @client_id,
+      authorization_endpoint: authorization_endpoint(),
+      client_id: client_id(),
       redirect_uri: redirect_uri,
       state: state,
-      scopes: ["openid", "offline_access", "email", "profile"],
-      resources: [],
-      prompt: @prompt
+      scopes: scopes(),
+      resources: resources(),
+      prompt: prompt()
     }
   end
 
-  def callback_url, do: @callback_url
+  def scopes, do: Application.get_env(:ex_logto, :scopes) || @default_scopes
+  def resources, do: Application.get_env(:ex_logto, :resources) || @default_resources
+  def prompt, do: Application.get_env(:ex_logto, :prompt) || @default_prompt
 
-  def client_id, do: @client_id
-  def client_secret, do: @client_secret
-  def token_endpoint, do: @token_endpoint
+  def callback_url, do: Application.get_env(:ex_logto, :callback_url)
+  def client_id, do: Application.get_env(:ex_logto, :client_id)
+  def client_secret, do: Application.get_env(:ex_logto, :client_secret)
 
-  def end_session_endpoint, do: @end_session_endpoint
+  def authorization_endpoint, do: Application.get_env(:ex_logto, :authorization_endpoint)
+  def token_endpoint, do: Application.get_env(:ex_logto, :token_endpoint)
 
-  def post_logout_redirect_url, do: @post_logout_redirect_url
-  def user_info_endpoint, do: @user_info_endpoint
+  def end_session_endpoint, do: Application.get_env(:ex_logto, :end_session_endpoint)
+
+  def post_logout_redirect_url, do: Application.get_env(:ex_logto, :post_logout_redirect_url)
+  def user_info_endpoint, do: Application.get_env(:ex_logto, :user_info_endpoint)
 end
