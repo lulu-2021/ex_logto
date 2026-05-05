d4b3ed55e41898731ed0b2dc8c81858c4df02d2e
netflakes <you@example.com>
2026-05-05 16:06:29 +1000

    small change that forces a return url with a scheme of https

--- lib/ex_logto/url_utils.ex
+++ lib/ex_logto/url_utils.ex
@@ -6,10 +6,10 @@ defmodule ExLogto.UrlUtils do
 
   def clean_url("https", host, _port, path), do: "https://#{host}#{path}"
 
-  def clean_url(_scheme, host, port, path), do: "https://#{host}#{path}"
+  def clean_url(_scheme, host, _port, path), do: "https://#{host}#{path}"
 
   def clean_url("https", host, _port, path, queries), do: "https://#{host}#{path}?#{queries}"
 
-  def clean_url(_scheme, host, port, path, queries),
-    do: "https://#{host}:#{port}#{path}?#{queries}"
+  def clean_url(_scheme, host, _port, path, queries),
+    do: "https://#{host}#{path}?#{queries}"
 end
